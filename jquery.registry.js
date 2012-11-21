c0d0d0e2e94971a190fb28536e6d771216d0ab99
maxschuster <maxschuster@MacPro>
2012-11-21 10:42:55 +0100

    - Added optional jQuery Tiny Pub/Sub support. - Added minified script

--- jquery.registry.js
+++ jquery.registry.js
@@ -14,18 +14,51 @@
  * limitations under the License.
  */
 
-(function($){
+(function($) {
+
+    var registry = {};
     
-    var registry = {},
-        $reg = $({});
+    /**
+     * Global registry for values
+     * @param {String} app Name of the app
+     * @param {String|Array|Object} key
+     * If String:
+     * Certain key to get or set
+     * If Array:
+     * List of keys to get from registry, like ['key1', 'key2']
+     * If Object:
+     * List of keys and values to set in the registry, like
+     * {'key1':1, 'key2':2}
+     * @param {*} value New value
+     * If key is String value will be used as value for this key,
+     * otherwise it must not be set
+     * @returns {String|Object|undefined}
+     */
+    $.registry = function(app) {
+        /**
+         * Check jQuery Tiny Pub/Sub support
+         */
+        var pubsub = typeof $.publish === 'function';
         
-    $.registry = function () {
+        /**
+         * Gets the value of a certain key from the registry
+         * @param {String} app Name of the app
+         * @param {String} key Certain key
+         * @returns {*|undefined} Value of key
+         */
         function getKey(app, key) {
             if (registry[app] && registry[app][key]) {
                 return registry[app][key];
             }
             return undefined;
         }
+
+        /**
+         * Sets the value of a certain key in the registry
+         * @param {String} app Name of the app
+         * @param {String} key Certain key
+         * @param {*} value New value
+         */
         function setKey(app, key, value) {
             var reg;
             if (!registry[app]) {
@@ -34,42 +67,39 @@
                 reg = registry[app];
             }
             if (reg[key] !== value) {
-                $reg.trigger(app + '/' + key + '.registry', {
-                    key: key,
-                    app: app,
-                    value: value
-                });
+                if (pubsub) {
+                    $.publish('/registry/' + app + '/' + key, {
+                        app: app,
+                        key: key,
+                        value: value
+                    });
+                }
                 reg[key] = value;
             }
-            
         }
-        var argCount = arguments.length,
-                app;
         
-        app = arguments[0];
-        
+        var argCount = arguments.length;
+
         if (argCount === 2) {
             if (typeof arguments[1] === 'string') {
                 return getKey(app, arguments[1]);
             } else if (Object.prototype.toString.call(arguments[1]) === '[object Array]') {
                 var result = {};
-                $.each(arguments[1], function(k,v){
+                $.each(arguments[1], function(k,v) {
                     result[v] = getKey(app, v);
                 });
                 return result;
             } else if (typeof arguments[1] === 'object') {
-                $.each(arguments[1], function(k,v){
+                $.each(arguments[1], function(k, v) {
                     setKey(app, k, v);
                 });
                 return;
             }
-        } else if (argCount === 3) {
+        } else if (argCount === 3 && typeof arguments[1] === 'string') {
             setKey(app, arguments[1], arguments[2]);
             return;
-        } else if (arguments[0] === 'event') {
-            return $reg;
         }
         throw new Error('Invalid arguments');
     };
-    
+
 })(jQuery);
